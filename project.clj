840d31acdd8822fde1b048861867921c0a611b70
Tim Reinke <tim@mostcallmetim.com>
2012-08-07 23:08:27 -0700

    Basic logging

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject soipf "0.0.1-SNAPSHOT"
             :description "A simple forum application"
             :dependencies [[org.clojure/clojure "1.3.0"]
+                           [org.clojure/tools.logging "0.2.4"]
                            [noir "1.3.0-beta8"]
                            [ring-anti-forgery "0.1.1"]
                            [congomongo "0.1.7"]
                            [clj-time "0.3.4"]
                            [org.pegdown/pegdown "1.1.0"]
-                           [midje "1.3.1"]]
-            :main soipf.server)
+                           [midje "1.3.1"]
+                           [log4j/log4j "1.2.17"]]
+            :main soipf.server
+            :uberjar-exclusions [#"META-INF/ECLIPSEF.SF"])
