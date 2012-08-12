32ec4d4b406165b65e38222640a4cab5b27fa481
Tim Reinke <tim@mostcallmetim.com>
2012-08-11 18:20:06 -0700

    Move test libraries to dev-dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,9 @@
                            [congomongo "0.1.11-SNAPSHOT"]
                            [clj-time "0.3.4"]
                            [org.pegdown/pegdown "1.1.0"]
-                           [midje "1.3.1"]
-                           [noir-test2 "1.0.0-SNAPSHOT"]
                            [log4j/log4j "1.2.17"]]
+            :profiles {:dev {:dependencies
+                             [[midje "1.4.0" :exclusions [org.clojure/clojure]]
+                              [noir-test2 "1.0.0-SNAPSHOT"]]}}
             :main soipf.server
             :uberjar-exclusions [#"META-INF/ECLIPSEF.SF"])
