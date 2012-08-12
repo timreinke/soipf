2828cc633fa22a39a8295121f681a8b4b2b604af
Tim Reinke <tim@mostcallmetim.com>
2012-08-11 17:41:45 -0700

    Bump mongo version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                            [org.clojure/tools.logging "0.2.4"]
                            [noir "1.3.0-beta8"]
                            [ring-anti-forgery "0.1.1"]
-                           [congomongo "0.1.7"]
+                           [congomongo "0.1.11-SNAPSHOT"]
                            [clj-time "0.3.4"]
                            [org.pegdown/pegdown "1.1.0"]
                            [midje "1.3.1"]
