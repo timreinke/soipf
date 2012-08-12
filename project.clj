ae658906c3c2f712ce0a96a7f4c275da76318261
Tim Reinke <tim@mostcallmetim.com>
2012-08-12 01:05:39 -0700

    Remove ring exclusions; set checksum to warn for pegdown

--- project.clj
+++ project.clj
@@ -3,11 +3,7 @@
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojure/tools.logging "0.2.4"]
                            [noir "1.3.0-beta8"]
-                           [ring-anti-forgery "0.1.1"
-                            :exclusions [ring/ring-core
-                                         ring/ring-devel
-                                         ring/jetty-adapter
-                                         ring]]
+                           [ring-anti-forgery "0.1.1"]
                            [congomongo "0.1.11-SNAPSHOT"]
                            [clj-time "0.3.4"]
                            [org.pegdown/pegdown "1.1.0"]
@@ -16,4 +12,5 @@
                              [[midje "1.4.0" :exclusions [org.clojure/clojure]]
                               [noir-test2 "1.0.0-SNAPSHOT"]]}}
             :main soipf.server
+            :checksum :warn
             :uberjar-exclusions [#"META-INF/ECLIPSEF.SF"])
