e909da6c210a748e7ea67698918d3b561f310aee
Tim Reinke <tim@mostcallmetim.com>
2013-01-19 23:17:16 -0800

    Noir forcibly ejected.  Application doesn't compile.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
             :description "A simple forum application"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojure/tools.logging "0.2.4"]
-                           [noir "1.3.0-beta8"]
                            [ring-anti-forgery "0.1.1"]
                            [congomongo "0.1.11-SNAPSHOT"]
                            [clj-time "0.3.4"]
@@ -14,8 +13,7 @@
                            [log4j/log4j "1.2.17"]]
             :profiles {:dev
                        {:dependencies
-                        [[midje "1.4.0" :exclusions [org.clojure/clojure]]
-                         [noir-test2 "1.0.0-SNAPSHOT"]]
+                        [[midje "1.4.0" :exclusions [org.clojure/clojure]]]
                         :plugins
                         [[lein-midje "2.0.0-SNAPSHOT"]]}}
             :main soipf.server
