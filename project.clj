9c08c8dedb18c31b9ae96cb9861c002b0c27614f
Tim Reinke <tim@mostcallmetim.com>
2013-01-25 22:10:16 -0800

    Add datomic; remove unneeded dependencies; bump lein-midje

--- project.clj
+++ project.clj
@@ -3,19 +3,15 @@
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [org.clojure/tools.logging "0.2.4"]
                            [ring-anti-forgery "0.1.1"]
-                           [congomongo "0.1.11-SNAPSHOT"]
                            [clj-time "0.3.4"]
+                           [com.datomic/datomic-free "0.8.3767"]
                            [org.pegdown/pegdown "1.1.0"]
-                           [org.clojars.hozumi/mongodb-session "1.0.1"
-                            :exclusions [org.clojure/clojure
-                                         ring/ring-core
-                                         congomongo]]
                            [log4j/log4j "1.2.17"]]
             :profiles {:dev
                        {:dependencies
                         [[midje "1.4.0" :exclusions [org.clojure/clojure]]]
                         :plugins
-                        [[lein-midje "2.0.0-SNAPSHOT"]]}}
+                        [[lein-midje "2.0.4"]]}}
             :main soipf.server
             :checksum :warn
             :uberjar-exclusions [#"META-INF/ECLIPSEF.SF"])
