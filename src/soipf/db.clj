fc7dc8a069315235a51b6cd17ff3e7ad6aa55fa8
Tim Reinke <tim@mostcallmetim.com>
2013-01-30 19:41:34 -0800

    Make soipf.db/fetch simpler

--- src/soipf/db.clj
+++ src/soipf/db.clj
@@ -1,29 +1,16 @@
 (ns soipf.db
   (:require [datomic.api :as d :refer [q db]]))
 
-(def conn nil)
+(declare conn)
 
 (defn set-connection-globally [uri]
   (alter-var-root conn (fn [_] (d/connect uri))))
 
-(defn make-where
-  "Given a map `matches` of attributes -> values, generate a list
-   of datalog where clauses on entity `entity-symbol`
-
-   (make-where '?e {:app.user/login \"user\"})
-   == '((?e :app.user/login \"user\"))"
-  [entity-symbol matches]
-  (map (fn [[k v]]
-         (list entity-symbol k v))
-       matches))
-
-(defn make-query [matches]
-  (list* :find '?e :where (make-where '?e matches)))
-
 (defn fetch
   "Returns the first result found by the map `matches` as a map"
-  [matches fields]
-  (let [results (q (make-query matches) (db conn))
+  [key value fields]
+  (let [results (q '[:find ?e :in $ ?key ?value :where [?e ?key ?value]]
+                   (db conn) key value)
         eid (ffirst results)
         entity (-> conn db (d/entity eid))]
     (into {} (map
